ceb1a63a37514af08c6936ad9f6d81d695f73e23
anastoffel <162522676+anastoffel@users.noreply.github.com>
2024-03-14 20:17:24 -0300

    Add files via upload

---
+++ aulinha.js
@@ -0,0 +1,33 @@
+// Exercicio 1
+let a = 20
+let b = 50
+let soma = (a + b)
+console.log (soma)
+// Exercicio 2
+let nota1 = 5
+let nota2 = 7
+let nota3 = 8
+let media = (nota1 + nota2 + nota3 /3)
+console.log (media)
+// Exercicio 3
+let C = 12
+let F = (C * 9/5 + 32)
+console.log (F)
+// Exercicio 4
+let base = 20
+let altura = 5
+let area = (base * altura)
+console.log (area)
+// Exercicio 5
+let nome = "Ana"
+let sobrenome = " Stoffel"
+let nomecompleto = (nome + sobrenome)
+console.log (nomecompleto)
+// Exercicio 6
+let numero1 = "50"
+let numero2 = "25"
+let sominha = (numero1 + numero2)
+console.log (sominha)
+Number (numero1)
+Number (numero2)
+console.log (Number (numero1) + Number (numero2))
